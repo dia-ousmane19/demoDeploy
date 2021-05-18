32fccd98491b30411832eb1992c1f6c62f886e1d
dia-ousmane19 <ousmanedia846@gmail.com>
2021-05-18 11:49:50 +0000

    ad tes

--- config/prod/deploy.php
+++ config/prod/deploy.php
@@ -8,9 +8,9 @@ return new class extends DefaultDeployer
     {
         return $this->getConfigBuilder()
             // SSH connection string to connect to the remote server (format: user@host-or-IP:port-number)
-            ->server('aroundp2@mocha3035')
+            ->server('aroundp2@aroundphp.net:4747')
             // the absolute path of the remote server directory where the project is deployed
-            ->deployDir('/home/aroundph/public_html')
+            ->deployDir('/home/aroundp2/public_html/')
             // the URL of the Git repository where the project code is hosted
             ->repositoryUrl('git@github.com:dia-ousmane19/demoDeploy.git')
             // the repository branch to deploy
